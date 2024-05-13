79b3d1092c6104ca61da6bc2da4309fc8b91f732
Miguel Cid <miguel.cid@tum.de>
2024-05-13 15:52:27 +0200

    Task 2: Prototype finished.

--- submissions/ex02/src/prototype/commands/BookCopies.java
+++ submissions/ex02/src/prototype/commands/BookCopies.java
@@ -1,15 +1,25 @@
 package prototype.commands;
 
+import java.time.LocalDate;
+
 public class BookCopies {
     public static void importBookCopy(String title, String author, String year, String genre) {
         System.out.println("Importing...");
     }
     public static void borrow(String bookId, String userId, String borrowDays) {
         // LocalDate returnDate = LocalDate.now().plusDays(Integer.parseInt(borrowDays));
+        // Write the returnDate in a file to store it
         System.out.println("Borrowing...");
+    }
+    public static void overdueFee(String userId) {
+        // If Localdate.now is after the returnDate then it applies a fee.
+        // Read the returnDate in the file and see if the Customer need to pay a fee.
+        System.out.println("Overdue...");
     }
 
     public static void returnBook(String bookId, String userId) {
         System.out.println("Returning...");
+        // If time is exceeded, then we call the overdueFee method with userId parameter and the date it should be returned.
+        overdueFee(userId);
     }
 }
