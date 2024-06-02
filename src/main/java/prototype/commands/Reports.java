ed228787d04764b516815908000750856269022b
urkocornejo <urko.cornejo@tum.de>
2024-06-02 16:58:55 +0200

    Corrected Reports.java according to the new structure

--- src/main/java/prototype/commands/Reports.java
+++ src/main/java/prototype/commands/Reports.java
@@ -10,59 +10,61 @@ public class Reports {
         }
     }
     public static void allBorrowedCopies(){
-        for (Map.Entry<Integer, Integer> entry : BookCopy.getCopyBorrowers().entrySet()) {
-            int copyId = entry.getKey();
-            String isbn = BookCopy.copyIdToIsbn(copyId);
-            Book book = Book.getBookByIsbn(isbn);
-            System.out.println("Title: "+book.getTitle()+"; Author: "+book.getAuthor()+
-                    "; Year: "+book.getYear()+"; ISBN: "+book.getIsbn()+ "; Copy ID: "+copyId);
+        for (BookCopy bookCopy : BookCopy.getBookCopies()) {
+            if (bookCopy.isBorrowed()) {
+                Book book = Book.getBookByIsbn(bookCopy.getIsbn());
+                assert book != null;
+                System.out.println("Title: "+book.getTitle()+"; Author: "+book.getAuthor()+
+                        "; Year: "+book.getYear()+"; ISBN: "+book.getIsbn()+ "; Copy ID: "+bookCopy.getCopyId());
+            }
+
         }
     }
     public static void allNonBorrowedCopies(){
-        for (Map.Entry<Integer, Boolean> entry : BookCopy.getBorrowStatus().entrySet()) {
-            int copyId = entry.getKey();
-            boolean status = entry.getValue();
-            if (!status) {
-                String isbn = BookCopy.copyIdToIsbn(copyId);
-                if (BookCopy.getCopyToBookMap().containsValue(isbn)) {
-                    Book book = Book.getBookByIsbn(isbn);
-                    System.out.println("Title: " + book.getTitle() + "; Author: " + book.getAuthor() +
-                            "; Year: " + book.getYear() + "; ISBN: " + book.getIsbn() + "; Copy ID: " + copyId);
-                } else {
-                    System.out.println("No book found!");
-                }
+        for (BookCopy bookCopy : BookCopy.getBookCopies()) {
+            if (!bookCopy.isBorrowed()) {
+                Book book = Book.getBookByIsbn(bookCopy.getIsbn());
+                assert book != null;
+                System.out.println("Title: "+book.getTitle()+"; Author: "+book.getAuthor()+
+                        "; Year: "+book.getYear()+"; ISBN: "+book.getIsbn()+ "; Copy ID: "+bookCopy.getCopyId());
             }
+
         }
     }
     public static void allCustomers(){
         for (Customer customer : Customer.getCustomers()) {
             String status;
+            int userId = customer.getUserId();
+            int booksBorrowed=0;
+
             if (customer.isPaymentStatus()) {
                 status = "Paid";
             } else {
                 status = "Not Paid";
             }
+
+            for (BookCopy bookCopy : BookCopy.getBookCopies()) {
+                if (userId == bookCopy.getUserId() && bookCopy.isBorrowed()) {
+                    booksBorrowed++;
+                }
+            }
             System.out.println("User ID: " + customer.getUserId() + "; First Name: " + customer.getFirstName() +
                     "; Name: " + customer.getName() + "; Payment Status: " +status+"; Number of Books Currently Borrowed: "
-                    + BookCopy.amountOfBooksPerCustomer(BookCopy.getCopyBorrowers(), customer.getUserId()));
+                    + booksBorrowed);
+
         }
 
     }
     public static void BorrowedCustomer(int userId){
-        for (Map.Entry<Integer, Integer> entry : BookCopy.getCopyBorrowers().entrySet()) {
-            int copyId = entry.getKey();
-            int entryUserId = entry.getValue();
+        for (BookCopy bookCopy : BookCopy.getBookCopies()) {
 
-            if (userId == entryUserId) {
-                String isbn = BookCopy.copyIdToIsbn(copyId);
-                Book book = Book.getBookByIsbn(isbn);
-                if (BookCopy.getCopyToBookMap().containsValue(isbn)) {
-                    System.out.println("Title: " + book.getTitle() + "; Author: " + book.getAuthor() +
-                            "; Year: " + book.getYear() + "; ISBN: " + book.getIsbn() + "; Copy ID: " + copyId);
-                } else {
-                    System.out.println("No book found!");
-                }
+            if (userId == bookCopy.getUserId()) {
+                Book book = Book.getBookByIsbn(bookCopy.getIsbn());
+                assert book != null;
+                System.out.println("Title: " + book.getTitle() + "; Author: " + book.getAuthor() +
+                        "; Year: " + book.getYear() + "; ISBN: " + book.getIsbn() + "; Copy ID: " + bookCopy.getCopyId());
             }
         }
     }
+
 }
